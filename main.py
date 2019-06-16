f817b1fa4b803ce15b21fa9bf3778995aba0c346
marcou49 <richard.casares@gmail.com>
2019-06-16 17:06:19 +0200

    Actualización textos más aumento del número random

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def index():
     if request.method == "GET":
         nombre = request.cookies.get("nombre")
         apellido = request.cookies.get("apellido")
-        intentos = request.cookies.get("intentos")
+        intentos = request.cookies.get("intentos") #cogemos nombre, apellidos y número de intentos desde cookies
 
         return render_template("index.html", nombre=nombre, apellido=apellido, intentos=intentos)
 
@@ -20,17 +20,17 @@ def index():
         nombre = request.form.get("nombre")
         apellido = request.form.get("apellido")
         oculto = request.cookies.get("oculto")
-        intentos = '0'
+        intentos = '0' #empieza el juego intentos a cero
 
         response = make_response(render_template("index.html", nombre=nombre, apellido=apellido, intentos=intentos))
         response.set_cookie("nombre", nombre)
         response.set_cookie("apellido", apellido)
-        response.set_cookie("intentos", str(intentos))
+        response.set_cookie("intentos", str(intentos)) #mandamos nombre, apellidos e intentos para recuento y mensajes
 
 
         if not oculto:
-            nuevo_oculto = random.randint(1, 5)
-            response.set_cookie("oculto", str(nuevo_oculto))
+            nuevo_oculto = random.randint(1, 20)
+            response.set_cookie("oculto", str(nuevo_oculto)) # si es la primera vez que llega aquí generamos oculto
 
         return response
 
@@ -43,11 +43,11 @@ def result():
 
     if secreto_usuario == oculto:
         intentos = request.cookies.get("intentos")
-        new_intentos = int(intentos) + 1
+        new_intentos = int(intentos) + 1 # generamos nuevo numero de intentos desde la cookie + 1
         mensaje = "Muy bien {0}, acertaste".format(str(nombre))
         palmares = "Acertaste en {0} intentos".format(str(new_intentos))
         response = make_response(render_template("result.html", mensaje=mensaje, nombre=nombre, new_intentos=new_intentos, intentos=intentos, palmares=palmares))
-        response.set_cookie("oculto", str(random.randint(1, 5)))
+        response.set_cookie("oculto", str(random.randint(1, 20)))
         response.set_cookie("intentos", str(0))
 
         return response
@@ -55,17 +55,17 @@ def result():
 
     elif secreto_usuario > oculto:
         intentos = request.cookies.get("intentos")
-        new_intentos = int(intentos) + 1
-        mensaje = "Espabila {0} es más chico".format(str(nombre))
+        new_intentos = int(intentos) + 1 # añadimos un intento
+        mensaje = "No {0}, el numero secreto es más pequeño".format(str(nombre))
         response = make_response(render_template("result.html", mensaje=mensaje, nombre=nombre, new_intentos=new_intentos))
-        response.set_cookie("intentos", str(new_intentos))
+        response.set_cookie("intentos", str(new_intentos)) #actualizamos cookie de intentos
 
         return response
 
     elif secreto_usuario < oculto:
         intentos = request.cookies.get("intentos")
         new_intentos = int(intentos) + 1
-        mensaje = "Espabila {0} es más tocho".format(str(nombre))
+        mensaje = "No {0}, el número secreto es más grande".format(str(nombre))
         response = make_response(render_template("result.html", mensaje=mensaje, nombre=nombre, new_intentos=new_intentos))
         response.set_cookie("intentos", str(new_intentos))
 
